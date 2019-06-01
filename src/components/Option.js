69adfe1ee2794af83820b870f2bc3fe36f6eaa0b
Reece Pritchard <rapritchard96@gmail.com>
2019-06-01 12:08:03 +0100

    Updated code to make use of imports and exports.

---
+++ src/components/Option.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Option = props => (
+  <li>
+    {props.optionText}
+    <button
+      onClick={(e) => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+    Remove
+    </button>
+  </li>
+);
+
+export default Option;
--- src/utils.js
+++
@@ -1,1 +0,0 @@
-console.log('utilis.js is running');
